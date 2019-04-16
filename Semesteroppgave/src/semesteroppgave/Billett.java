6bff8074ff091dff5b92dc56e730bda7a12a57bc
sarahbra <sarah.rastad@gmail.com>
2019-04-16 10:34:22 +0200

    Endret arrangement

--- Semesteroppgave/src/semesteroppgave/Billett.java
+++ Semesteroppgave/src/semesteroppgave/Billett.java
@@ -3,11 +3,11 @@ package semesteroppgave;
 public class Billett {
     private Lokale sted;
     private int plassNr;
-    private Tidspunkt dato;
+    private Dato dato;
     private int pris;
     private Person kjøper;
     
-    public Billett(Lokale sted, int plassNr, Tidspunkt dato, int pris, 
+    public Billett(Lokale sted, int plassNr, Dato dato, int pris, 
             Person kjøper) {
         this.sted = sted;
         this.plassNr = plassNr;
@@ -20,8 +20,8 @@ public class Billett {
     public Lokale getSted() {return sted;}
     public void setPlassNr(int plassNr) {this.plassNr = plassNr;}
     public int getPlassNr() {return plassNr;}
-    public void setTidspunkt(Tidspunkt dato) {this.dato = dato;}
-    public Tidspunkt getTidspunkt() {return dato;}
+    public void setDato(Dato dato) {this.dato = dato;}
+    public Dato getDato() {return dato;}
     public void setPris(int pris) {this.pris = pris;}
     public int getPris() {return pris;}
     public void setPerson(Person kjøper) {this.kjøper = kjøper;}
@@ -29,30 +29,23 @@ public class Billett {
     
     @Override
     public String toString() {
-        return String.format("Sted: %d \nDato: %d \nPlassNr: %d \nPris: %d"
-                + "\nKjøper: %d", sted, dato, plassNr, pris, kjøper);
+        return String.format("Sted: %s \nDato: %d"
+                + "\nPlassNr: %d \nPris: %d"
+                + "\nKjøper: %s", sted, dato, plassNr, pris, kjøper);
     }
 }
             
-class Tidspunkt {
-    private int time;
-    private int minutt;
+class Dato {
     private int år;
     private int måned;
     private int dag;
     
-    public Tidspunkt(int time, int minutt, int år, int måned, int dag) {
-        this.time = time;
-        this.minutt = minutt;
+    public Dato(int år, int måned, int dag) {
         this.år = år;
         this.måned = måned;
         this.dag = dag;
     }
     
-    public void setTime(int time) {this.time = time;}
-    public int getTime() {return time;}
-    public void setMinutt(int minutt) {this.minutt = minutt;}
-    public int getMinutt() {return minutt;}
     public void setÅr(int år) {this.år = år;}
     public int getÅr() {return år;}
     public void setMåned(int måned) {this.måned = måned;}
@@ -62,6 +55,6 @@ class Tidspunkt {
     
     @Override
     public String toString() {
-        return String.format("%d/%d - %d %d:%d", dag, måned, år, time, minutt);
+        return String.format("%d/%d - %d", dag, måned, år);
     }
 }
