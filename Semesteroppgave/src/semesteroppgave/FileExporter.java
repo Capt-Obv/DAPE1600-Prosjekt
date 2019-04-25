ba666ed8f6204d1deffe03df689f2bc5dc551030
sarahbra <sarah.rastad@gmail.com>
2019-04-25 20:35:53 +0200

    Jobber med skrivTilFil

---
+++ Semesteroppgave/src/semesteroppgave/FileExporter.java
@@ -0,0 +1,39 @@
+
+package semesteroppgave;
+
+/**
+ *
+ * @author sarah
+ */
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExporter {
+abstract void writeDeltakerToFile(FileWriter file, Deltaker pers);
+
+    public static void writeToFile(String objType, String filepath) {
+        try { 
+            FileWriter file = new FileWriter(new File(filepath)); 
+            if (objType.toUpperCase().equals("DELTAKER")) {
+                for(Deltaker pers: main.deltakerListe) {
+                    writeDeltakerToFile(file, pers);
+                }
+            } else if(objType.toUpperCase().equals("LOKALE")) {
+                
+            } else if(objType.toUpperCase().equals("ARRANGEMENT")) {
+                
+            } else if(objType.toUpperCase().equals("KONTAKTPERSON")) {
+                
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            // Gjør noe
+        } catch (IOException e) { 
+        // Gjøre noe 
+    } 
+} 
