6acdb165d4552cdad7d7c0b85360cd2103aa3dc7
sarahbra <sarah.rastad@gmail.com>
2019-04-25 21:11:40 +0200

    Endret lokale

--- Semesteroppgave/src/semesteroppgave/Lokale.java
+++ Semesteroppgave/src/semesteroppgave/Lokale.java
@@ -8,8 +8,9 @@ package semesteroppgave;
 public class Lokale {
     private String navn;
     private final int antPlasser;
+    private String type;
     
-    public Lokale(String navn, int antPlasser) {
+    public Lokale(String navn, int antPlasser, String type) {
         this.navn = navn;
         this.antPlasser = antPlasser;
     }
@@ -17,55 +18,36 @@ public class Lokale {
     public void setNavn(String navn) {this.navn = navn;}
     public String getNavn() {return navn;}
     public int getAntPlasser() {return antPlasser;}
+    public String getType() {return type;}
     
     @Override
     public String toString() {
-        return String.format("Lokalets navn: %s \nKapasitet: %d",
-                 navn, antPlasser);
+        return String.format("Lokalets navn: %s \nKapasitet: %d \nType: %s",
+                 navn, antPlasser, type);
     }
 }
 
 class Kino extends Lokale {
     public Kino(String navn, int antPlasser) {
-        super(navn, antPlasser);
-    }
-    
-    @Override
-    public String toString() {
-            return super.toString() + "\nType: kino.";
+        super(navn, antPlasser, "kino");
     }
 }
 
 class Konsertsal extends Lokale {
-    
+    private String type = "konsertsal";
     public Konsertsal(String navn, int antPlasser) {
-        super(navn, antPlasser);
-    }
-    @Override
-    public String toString() {
-            return super.toString() + "\nType: konsertsal.";
+        super(navn, antPlasser,"konsertsal");
     }
 }
 
 class Foredragsal extends Lokale {
-    
     public Foredragsal(String navn, int antPlasser) {
-        super(navn, antPlasser);
-    }
-    
-    @Override
-    public String toString() {
-            return super.toString() + "\nType: foredragsal.";
+        super(navn, antPlasser, "foredragsal");
     }
 }
 
 class Teatersal extends Lokale {
     public Teatersal(String navn, int antPlasser) {
-        super(navn, antPlasser);
-    }
-    
-    @Override
-    public String toString() {
-        return super.toString() + "\nType: teatersal.";
+        super(navn, antPlasser, "teatersal");
     }
 }
