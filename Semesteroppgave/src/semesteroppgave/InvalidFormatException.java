d449aa7a405633fe578bad7d5f9397be5b9601cf
sarahbra <sarah.rastad@gmail.com>
2019-04-30 20:03:37 +0200

    Exception-handling

--- Semesteroppgave/src/semesteroppgave/InvalidFormatException.java
+++ Semesteroppgave/src/semesteroppgave/InvalidFormatException.java
@@ -1,6 +1,11 @@
 package semesteroppgave;
 import java.lang.Exception;
 
+/* File contains various subclasses of Exception which use 
+    the built in constructor of the super class with the
+    error-message of the given exception as a parameter
+*/
+
 public class InvalidFormatException extends Exception {
     public InvalidFormatException(String msg) {
         super(msg);
