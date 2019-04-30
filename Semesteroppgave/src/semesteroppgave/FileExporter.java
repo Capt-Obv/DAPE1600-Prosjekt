61a97513b6f95e64ac0b1c7ca7b7c2b60639baf7
sarahbra <sarah.rastad@gmail.com>
2019-04-30 19:45:54 +0200

    Fikset for jobj-fil

--- Semesteroppgave/src/semesteroppgave/FileExporter.java
+++ Semesteroppgave/src/semesteroppgave/FileExporter.java
@@ -11,32 +11,80 @@ import java.nio.file.Paths;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import static semesteroppgave.Semesteroppgave.main;
 
 abstract class FileExporter {
 abstract void writeDeltakerToFile(FileWriter file, Deltaker pers) throws FileNotFoundException,
         IOException;
+abstract void writeLokaleToFile(FileWriter file, Lokale lok) throws FileNotFoundException,
+        IOException;
+abstract void writeArrangementToFile(FileWriter file, Arrangement arr) throws FileNotFoundException,
+        IOException;
+abstract void writeKontaktpersonToFile(FileWriter file, Kontaktperson kontakt) throws FileNotFoundException,
+        IOException;
 
-    public void writeToFile(String objType, String filepath) {
-        /*
-        try { 
-            FileWriter file = new FileWriter(new File(filepath)); 
-            if (objType.toUpperCase().equals("DELTAKER")) {
-                for(Deltaker pers: main.deltakerListe) {
+    public void writeToFile(String objType, String filename, String fileType) throws FileNotFoundException,
+             IOException, ClassNotFoundException, InvalidObjectTypeException {
+        FileWriter file = null;
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        if(objType.equals("csv")) {
+            file = new FileWriter(new File(filename));
+        } else if(objType.equals("jobj")) {
+            fos = new FileOutputStream(new File(filename));
+        } else {
+            throw new FileNotFoundException("Filetype must be either .csv or .jobj");
+        }
+        
+        if (objType.toUpperCase().equals("DELTAKER")) {
+            for(Deltaker pers: main.deltakerListe) {
+                if(objType.equals("csv")) {
                     writeDeltakerToFile(file, pers);
+                } else {
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(pers);
                 }
-            } else if(objType.toUpperCase().equals("LOKALE")) {
-                
-            } else if(objType.toUpperCase().equals("ARRANGEMENT")) {
-                
-            } else if(objType.toUpperCase().equals("KONTAKTPERSON")) {
-                
             }
+        } else if(objType.toUpperCase().equals("LOKALE")) {
+            for(Lokale lok: main.lokaleListe) {
+                if(objType.equals("csv")) {
+                    writeLokaleToFile(file, lok);
+                } else {
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(lok);
+                }
+            }    
+        } else if(objType.toUpperCase().equals("ARRANGEMENT")) {
+            for(Arrangement arr: main.arrangementListe) {
+                if(objType.equals("csv")) {
+                    writeArrangementToFile(file, arr);
+                } else {
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(arr);
+                }
+            }    
+        } else if(objType.toUpperCase().equals("KONTAKTPERSON")) {
+            for(Kontaktperson pers: main.kontaktpersonListe) {
+                if(objType.equals("csv")) {
+                    writeKontaktpersonToFile(file, pers);
+                } else {
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(pers);
+                }
+            }    
+        } else {
+            throw new InvalidObjectTypeException("Not a valid objecttype");
+        }
+        
+        if(file!=null) {
             file.close();
-        } catch (FileNotFoundException e) {
-            // Gjør noe
-        } catch (IOException e) { 
-        // Gjøre noe */
+        }
+        if(fos!=null) {
+            fos.close();
+        }
     } 
 }
 
