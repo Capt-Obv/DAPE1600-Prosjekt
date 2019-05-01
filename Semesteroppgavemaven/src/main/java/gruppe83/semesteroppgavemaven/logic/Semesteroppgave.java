0821b6a653357eb078a6048bd839aec7008f347b
Capt-Obv <45256527+Capt-Obv@users.noreply.github.com>
2019-05-01 15:09:44 +0200

    test

---
+++ Semesteroppgavemaven/src/main/java/gruppe83/semesteroppgavemaven/logic/Semesteroppgave.java
@@ -0,0 +1,32 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppe83.semesteroppgavemaven.logic;
+
+/**
+ *
+ * @author sarah
+ */
+public class Semesteroppgave {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        /*
+        Kontaktperson lise = new Kontaktperson("Line Linesen", 22301947, 
+                "epost@gmail.com", "En jente som liker filmer");
+        Kino kino1 = new Kino("Ringen", 101);
+        Dato dag = new Dato(20,10,2002);
+        Arrangement fest = new Arrangement("Fjortiskino", kino1, lise, dag, 150);
+        Person petter = new Person("Petter Persen", 99403295);
+        fest.billettsalg(petter);
+        System.out.println(fest);
+*/
+    }
+    
+}
