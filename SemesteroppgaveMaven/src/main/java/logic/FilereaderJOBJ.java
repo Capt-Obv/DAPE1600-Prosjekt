52dea90a1c0e011124d3b3a964be4a15b80e1fab
sarahbra <sarah.rastad@gmail.com>
2019-05-01 20:20:43 +0200

    Fiksa import

---
+++ SemesteroppgaveMaven/src/main/java/logic/FilereaderJOBJ.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author sarah
+ */
+public class FilereaderJOBJ extends Filereader{
+    
+    @Override
+    public void initializeImport(String filename, String objType) throws 
+            FileNotFoundException, IOException, ClassNotFoundException, InvalidTimeOverlapException, 
+            InvalidObjectTypeException, InvalidFormatException {
+        readFile(filename, objType, "JOBJ");
+    }
+}
