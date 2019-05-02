9ce7ad287879b28fa3a5adf10f485ab44c6d014d
sarahbra <sarah.rastad@gmail.com>
2019-05-02 17:49:25 +0200

    Fixes

--- SemesteroppgaveMaven/src/main/java/logic/FilereaderCSV.java
+++ SemesteroppgaveMaven/src/main/java/logic/FilereaderCSV.java
@@ -7,19 +7,13 @@ import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-/*
+
 public class FilereaderCSV extends Filereader {
 
-    @Override
-<<<<<<< HEAD
-    public void initializeImport(File file, String objType) throws 
-            FileNotFoundException, IOException, ClassNotFoundException, InvalidTimeOverlapException, 
-=======
-    public void initializeImport(String filename, String objType) throws
+    @Override 
+    public void initializeImport(File filename, String objType) throws
             FileNotFoundException, IOException, ClassNotFoundException, InvalidTimeOverlapException,
->>>>>>> 6295515fdb7b3afb38a48b82714d171776b65fbf
             InvalidObjectTypeException, InvalidFormatException {
         readFile(file, objType, "csv");
     }
 }
-*/
