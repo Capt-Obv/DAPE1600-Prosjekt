52646b8754ee72455ac11f96c495b4f8f36076ac
sarahbra <sarah.rastad@gmail.com>
2019-05-02 17:49:07 +0200

    Fixes

--- SemesteroppgaveMaven/src/main/java/logic/FilereaderJOBJ.java
+++ SemesteroppgaveMaven/src/main/java/logic/FilereaderJOBJ.java
@@ -19,12 +19,6 @@ public class FilereaderJOBJ extends Filereader{
     public void initializeImport(File file, String objType) throws 
             FileNotFoundException, IOException, ClassNotFoundException, InvalidTimeOverlapException, 
             InvalidObjectTypeException, InvalidFormatException {
-<<<<<<< HEAD
         readFile(file, objType, "jobj");
-=======
-        /*
-        readFile(filename, objType, "JOBJ");
-*/
->>>>>>> 6295515fdb7b3afb38a48b82714d171776b65fbf
     }
 }
